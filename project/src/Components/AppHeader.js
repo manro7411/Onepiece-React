dea00a093a260dc59b843c9c35968d5284dd7eb4
Ratchanon Traitiprat <teenoigammer2546@gmail.com>
2022-04-26 14:43:55 +0700

    Update file from tocal to repository

---
+++ project/src/Components/AppHeader.js
@@ -0,0 +1,10 @@
+/* eslint-disable jsx-a11y/alt-text */
+import "./AppHeader.css";
+function AppHeader() {
+    return(
+        <header className="app-header">
+        <img className='app-header-logo' src="/images/PngItem_2142197.png" / >
+      </header>
+    );    
+}
+export default AppHeader;
